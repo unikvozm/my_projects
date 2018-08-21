efb6d5c9167e1dfcb4ce75ca85b7aadc11fcc752
unikvozm <unikvozm@users.noreply.github.com>
2018-08-21 05:06:20 +0000

    20180821T050605Z

---
+++ mario.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <cs50.h>
+int main(void)
+{
+    int n;
+    do
+    {
+        n = get_int("Integer height: ");
+    }
+    while (n < 0 || n > 23);
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
+}
